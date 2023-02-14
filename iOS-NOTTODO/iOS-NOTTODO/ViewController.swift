499c1ea9e65dfe2d400d5142e73a93b666b7d3f3
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-02-15 06:23:08 +0900

    [Chore] 초기세팅 1. .gitignore 추가 2. issue templete, PR templete 생성 3. 프로젝트 파일 생성

---
+++ iOS-NOTTODO/iOS-NOTTODO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-NOTTODO
+//
+//  Created by 강윤서 on 2023/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
