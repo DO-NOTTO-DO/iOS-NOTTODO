f666ed5eeeb0df6fe29dbf9e1405a384602dd44a
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-02-17 05:23:45 +0900

    [Add] #3 - 탭 바 생성을 위한 파일 추가

---
+++ iOS-NOTTODO/iOS-NOTTODO/Global/Extensions/UIScreen+.swift
@@ -0,0 +1,8 @@
+//
+//  UIScreen+.swift
+//  iOS-NOTTODO
+//
+//  Created by 강윤서 on 2023/02/17.
+//
+
+import Foundation
---
+++ iOS-NOTTODO/iOS-NOTTODO/Global/Extensions/UIViewController+.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController+.swift
+//  iOS-NOTTODO
+//
+//  Created by 강윤서 on 2023/02/17.
+//
+
+import Foundation
