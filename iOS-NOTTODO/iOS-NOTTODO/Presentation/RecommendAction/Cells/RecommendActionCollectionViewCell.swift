44395354e37e636ba68b4c523af8ed2ad26c796d
minseo205 <rlaalstj312@naver.com>
2023-02-25 22:53:11 +0900

    [Add] #18 - 실천행동선택 뷰 생성

---
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/RecommendAction/Cells/RecommendActionCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  RecommendActionCollectionViewCell.swift
+//  iOS-NOTTODO
+//
+//  Created by 김민서 on 2023/02/25.
+//
+
+import Foundation
---
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/RecommendAction/Cells/RecommendActionHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  RecommendActionHeaderView.swift
+//  iOS-NOTTODO
+//
+//  Created by 김민서 on 2023/02/25.
+//
+
+import Foundation
