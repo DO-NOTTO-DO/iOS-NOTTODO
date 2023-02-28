42a751106db5126e2c44e16737d4ffbd67a771a9
jeongdung-eo <jeuniis2@gmail.com>
2023-03-01 01:27:10 +0900

    [Add] #20 - Compositional layout 코드 추가

---
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/Common/CompositionalLayout.swift
@@ -0,0 +1,23 @@
+//
+//  CompositionalLayout.swift
+//  iOS-NOTTODO
+//
+//  Created by JEONGEUN KIM on 2023/03/01.
+//
+
+import UIKit
+
+final class CompositionalLayout {
+
+    class func _vertical(_ itemWidth: NSCollectionLayoutDimension,_ itemHeight: NSCollectionLayoutDimension,_ groupWidth: NSCollectionLayoutDimension,_ groupHeight: NSCollectionLayoutDimension, count: Int, edge: NSDirectionalEdgeInsets?) -> NSCollectionLayoutSection{
+        let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: itemWidth, heightDimension: itemHeight))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: groupWidth, heightDimension: groupHeight), subitem: item, count: count )
+        return section(group, edge ?? NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+    }
+    
+    class func section(_ group: NSCollectionLayoutGroup,_ edge: NSDirectionalEdgeInsets) -> NSCollectionLayoutSection {
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = edge
+        return section
+    }
+}
