aefa239ca08d6b939a1ce1c3b4c932efd29872ec
jeongdung-eo <jeuniis2@gmail.com>
2023-03-06 01:12:28 +0900

    [Design] #23 - 성취뷰 구현

--- iOS-NOTTODO/iOS-NOTTODO/Presentation/Achievement/AchievementViewController.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/Achievement/AchievementViewController.swift
@@ -7,18 +7,63 @@
 
 import UIKit
 
-final class AchievementViewController: UIViewController {
+import FSCalendar
+import Then
+import SnapKit
 
+final class AchievementViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private lazy var safeArea = self.view.safeAreaLayoutGuide
+    
+    // MARK: - UI Components
+    
+    private let scrollView = UIScrollView()
+    private let achievementLabel = UILabel()
+    private let monthCalendar = CalendarView(calendarScope: .month, scrollDirection: .horizontal)
+    private let statisticsView = UIView()
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        setLayout()
     }
 }
+
+// MARK: - Methods
 
 extension AchievementViewController {
     private func setUI() {
         view.backgroundColor = .white
+        scrollView.backgroundColor = .gray5
+    }
+    private func setLayout() {
+        view.addSubview(scrollView)
+        scrollView.addSubviews(achievementLabel, monthCalendar, statisticsView)
+        
+        scrollView.snp.makeConstraints {
+            $0.edges.equalTo(safeArea)
+        }
+        achievementLabel.snp.makeConstraints {
+            $0.top.equalTo(safeArea).offset(23)
+            $0.leading.equalTo(safeArea).offset(21)
+        }
+        
+        monthCalendar.snp.makeConstraints {
+            $0.top.equalTo(achievementLabel.snp.bottom).offset(22)
+            $0.directionalHorizontalEdges.equalTo(safeArea).inset(15)
+            $0.height.equalTo((getDeviceWidth()-30)*1.1)
+        }
+        
+        statisticsView.snp.makeConstraints {
+            $0.top.equalTo(monthCalendar.snp.bottom).offset(20)
+            $0.directionalHorizontalEdges.equalTo(safeArea).inset(15)
+            $0.height.equalTo((getDeviceWidth()-30)*0.6)
+            $0.bottom.equalTo(scrollView.snp.bottom).inset(20)
+        }
+        
     }
 }
