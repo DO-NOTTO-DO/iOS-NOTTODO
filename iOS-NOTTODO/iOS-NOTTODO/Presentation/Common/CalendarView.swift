5933a89cecdbe58e5cdaa8391d21cbe5c1588b40
jeongdung-eo <jeuniis2@gmail.com>
2023-03-12 12:42:47 +0900

    [Chore] #22 - today button 이미지, 위치 수정

--- iOS-NOTTODO/iOS-NOTTODO/Presentation/Common/CalendarView.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/Common/CalendarView.swift
@@ -54,7 +54,7 @@ extension CalendarView {
             $0.text = Utils.DateFormatter(format: I18N.yearMonthTitle, date: Date())
         }
         todayButton.do {
-            $0.configuration?.image = UIImage(systemName: "return.right")
+            $0.configuration?.image = .icBackToday
             $0.configuration?.title = I18N.todayButton
             $0.configuration?.imagePadding = 2
             $0.configuration?.contentInsets = NSDirectionalEdgeInsets.init(top: 3, leading: 6, bottom: 2, trailing: 7)
@@ -91,7 +91,7 @@ extension CalendarView {
             todayButton.snp.makeConstraints {
                 $0.top.equalTo(yearMonthLabel.snp.top)
                 $0.trailing.equalToSuperview().inset(18)
-                $0.size.equalTo(CGSize(width: 75, height: 30))
+                $0.size.equalTo(CGSize(width: 60, height: 30))
             }
             
             calendar.snp.makeConstraints {
