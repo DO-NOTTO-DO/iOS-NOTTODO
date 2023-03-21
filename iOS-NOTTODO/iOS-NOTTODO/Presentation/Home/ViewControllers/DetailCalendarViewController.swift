cbfb279b9bfe0416189bc754ac625373b92c7cc4
jeongdung-eo <jeuniis2@gmail.com>
2023-03-21 14:24:30 +0900

    [Fix] #28 - 월간 캘린더 선택 범위 수정

--- iOS-NOTTODO/iOS-NOTTODO/Presentation/Home/ViewControllers/DetailCalendarViewController.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/Home/ViewControllers/DetailCalendarViewController.swift
@@ -88,14 +88,48 @@ extension DetailCalendarViewController: FSCalendarDelegate, FSCalendarDataSource
     }
     
     func calendar(_ calendar: FSCalendar, shouldSelect date: Date, at monthPosition: FSCalendarMonthPosition) -> Bool {
-     //   switch calendar.to
-        Utils.calendarSelected(today: today, date: date)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let convertDate = dateFormatter.string(from: date)
+        let stringDate: [String] = ["2023-03-23", "2023-03-25", "2023-03-26"]
+        if stringDate.contains(convertDate) { return false }
+        return Utils.calendarSelected(today: today, date: date)
     }
+    
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, fillDefaultColorFor date: Date) -> UIColor? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let convertDate = dateFormatter.string(from: date)
+        let stringDate: [String] = ["2023-03-23", "2023-03-25", "2023-03-26"]
+        if stringDate.contains(convertDate) { return UIColor.gray2 }
+        return .clear
+    }
+    
     func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleSelectionColorFor date: Date) -> UIColor? {
         Utils.calendarTitleColor(today: today, date: date, selected: true)
     }
     
     func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
         Utils.calendarTitleColor(today: today, date: date, selected: false)
+    }
+}
+extension String {
+    func toDate() -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        if let date = dateFormatter.date(from: self) {
+            return date
+        } else {
+            return nil
+        }
+    }
+}
+extension Date {
+    func toString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        return dateFormatter.string(from: self)
     }
 }
