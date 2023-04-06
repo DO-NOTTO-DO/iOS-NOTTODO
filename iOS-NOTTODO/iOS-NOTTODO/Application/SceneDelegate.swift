fbef4c3766a8c8ae6caecbd699bca4a79ca76aed
minseo205 <rlaalstj312@naver.com>
2023-04-06 22:42:36 +0900

    [Fix] #35 - rootView 변경

--- iOS-NOTTODO/iOS-NOTTODO/Application/SceneDelegate.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Application/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let window = UIWindow(windowScene: windowScene)
             window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
             
-            let rootViewController = RecommendViewController() // AddMissionViewController()
+            let rootViewController = SecondOnboardingViewController()
             let navigationController = UINavigationController(rootViewController: rootViewController)
             navigationController.isNavigationBarHidden = true
             window.rootViewController = navigationController
