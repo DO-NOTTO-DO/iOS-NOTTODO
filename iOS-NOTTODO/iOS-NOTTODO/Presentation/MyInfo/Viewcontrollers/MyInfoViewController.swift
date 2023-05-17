5edcae4a72a0003303bcb12b8a788d682e534796
jeongdung-eo <jeuniis2@gmail.com>
2023-05-17 12:39:15 +0900

    [Feat] #49 - 구글 이메일 보내기 기능 구현

--- iOS-NOTTODO/iOS-NOTTODO/Presentation/MyInfo/Viewcontrollers/MyInfoViewController.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/MyInfo/Viewcontrollers/MyInfoViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 import Then
 import SnapKit
+import MessageUI
 
 final class MyInfoViewController: UIViewController {
     
@@ -137,9 +138,9 @@ extension MyInfoViewController: UICollectionViewDelegate {
         switch indexPath.section {
         case 1:
             switch indexPath.item {
-            case 0 :
+            case 0:
                 Utils.myInfoUrl(vc: self, url: MyInfoURL.guid.url)
-            default :
+            default:
                 Utils.myInfoUrl(vc: self, url: MyInfoURL.quesition.url)
             }
         case 2:
@@ -148,11 +149,78 @@ extension MyInfoViewController: UICollectionViewDelegate {
                 Utils.myInfoUrl(vc: self, url: MyInfoURL.notice.url)
             case 2:
                 Utils.myInfoUrl(vc: self, url: MyInfoURL.service.url)
-            default :
-                Utils.myInfoUrl(vc: self, url: MyInfoURL.guid.url)
+            default: 
+                sendEmail()
             }
         default:
             return
         }
+    }
+}
+extension MyInfoViewController {
+    func sendEmail() {
+        if MFMailComposeViewController.canSendMail() {
+            let composeViewController = MFMailComposeViewController()
+            composeViewController.mailComposeDelegate = self
+            
+            let bodyString = """
+                                 
+                                 -------------------
+                                 
+                                 Device Model : \(self.getDeviceIdentifier())
+                                 Device OS : \(UIDevice.current.systemVersion)
+                                 App Version : \(self.getCurrentVersion())
+                                 
+                                 -------------------
+                                 """
+            
+            composeViewController.setToRecipients(["notodonow@gmail.com"])
+            composeViewController.setSubject("낫투두에게 문의해요")
+            composeViewController.setMessageBody(bodyString, isHTML: false)
+            
+            self.present(composeViewController, animated: true, completion: nil)
+        } else {
+            print("메일 보내기 실패")
+            let sendMailErrorAlert = UIAlertController(title: "메일 전송 실패", message: "메일을 보내려면 'Mail' 앱이 필요합니다. App Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요.", preferredStyle: .alert)
+            let goAppStoreAction = UIAlertAction(title: "App Store로 이동하기", style: .default) { _ in
+                // 앱스토어로 이동하기(Mail)
+                if let url = URL(string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    } else {
+                        UIApplication.shared.openURL(url)
+                    }
+                }
+            }
+            let cancleAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+            
+            sendMailErrorAlert.addAction(goAppStoreAction)
+            sendMailErrorAlert.addAction(cancleAction)
+            self.present(sendMailErrorAlert, animated: true, completion: nil)
+        }
+    }
+    // Device Identifier 찾기
+    func getDeviceIdentifier() -> String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        return identifier
+    }
+
+    // 현재 버전 가져오기
+    func getCurrentVersion() -> String {
+        guard let dictionary = Bundle.main.infoDictionary,
+              let version = dictionary["CFBundleShortVersionString"] as? String else { return "" }
+        return version
+    }
+}
+extension MyInfoViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
