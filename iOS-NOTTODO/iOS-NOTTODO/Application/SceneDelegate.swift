9f4b82d5ba359afc4f558f2a8451e3f286214090
minseo205 <rlaalstj312@naver.com>
2023-05-27 21:22:04 +0900

    [Feat] #57 - 로고 로티 연결

--- iOS-NOTTODO/iOS-NOTTODO/Application/SceneDelegate.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Application/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let window = UIWindow(windowScene: windowScene)
             window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
             
-            let rootViewController = TabBarController()
+            let rootViewController = LogoOnboardingViewController() //TabBarController()
             let navigationController = UINavigationController(rootViewController: rootViewController)
             navigationController.isNavigationBarHidden = true
             window.rootViewController = navigationController
---
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/Onboarding/ViewControllers/ValueOnboardingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ValueOnboardingViewController.swift
+//  iOS-NOTTODO
+//
+//  Created by 김민서 on 2023/05/27.
+//
+
+import Foundation
