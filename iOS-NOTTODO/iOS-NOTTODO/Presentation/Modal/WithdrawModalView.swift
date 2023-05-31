3a6054e77b99bf6084871365963b5d2be0250995
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-01 04:10:23 +0900

    [Design] #60 - 탈퇴설문모달 뷰 UI 구현

--- iOS-NOTTODO/iOS-NOTTODO/Presentation/Modal/WithdrawModalView.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/Modal/WithdrawModalView.swift
@@ -6,15 +6,88 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class WithdrawModalView: UIView {
+final class WithdrawModalView: UIView {
+    
+    // MARK: - UI Components
+    
+    private let modalImageView = UIImageView()
+    private let modalTitleLabel = UILabel()
+    private let modalSubTitleLabel = UILabel()
+    private var surveyButton = UIButton()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - View Life Cycle
+    
+    init() {
+        super.init(frame: .zero)
+        setUI()
+        setLayout()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension WithdrawModalView {
+    private func setUI() {
+        backgroundColor = .white
+        layer.cornerRadius = 15
+        modalImageView.image = .quit2
+        
+        modalTitleLabel.do {
+            $0.font = .Pretendard(.bold, size: 16)
+            $0.textColor = .gray2
+            $0.textAlignment = .center
+            $0.text = I18N.withdrawModalTitle
+            $0.numberOfLines = 0
+        }
+        
+        modalSubTitleLabel.do {
+            $0.font = .Pretendard(.regular, size: 13)
+            $0.textColor = .gray4
+            $0.textAlignment = .center
+            $0.text = I18N.withdrawModalSubtitle
+            $0.numberOfLines = 0
+        }
+        
+        surveyButton.do {
+            $0.setTitle(I18N.surveyButton, for: .normal)
+            $0.backgroundColor = .black
+            $0.layer.cornerRadius = 7
+            $0.titleLabel?.font = .Pretendard(.medium, size: 14)
+            $0.setTitleColor(.white, for: .normal)
+//            $0.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        }
+    }
+    
+    private func setLayout() {
+        addSubviews(modalImageView, modalTitleLabel, modalSubTitleLabel, surveyButton)
+        
+        modalImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(24)
+            $0.leading.equalToSuperview().inset(44)
+            $0.trailing.equalToSuperview().inset(54)
+            $0.height.equalTo(118)
+        }
+        
+        modalTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(modalImageView.snp.bottom).offset(10)
+            $0.centerX.equalToSuperview()
+        }
+        
+        modalSubTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(modalTitleLabel.snp.bottom).offset(19)
+            $0.centerX.equalToSuperview()
+        }
+        
+        surveyButton.snp.makeConstraints {
+            $0.top.equalTo(modalSubTitleLabel.snp.bottom).offset(33)
+            $0.directionalHorizontalEdges.equalToSuperview().inset(18)
+            $0.height.equalTo(50)
+            $0.bottom.equalToSuperview().inset(17)
+        }
+    }
 }
