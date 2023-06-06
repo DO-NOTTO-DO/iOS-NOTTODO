836b8e70a42eff34aa1a3f7ba76521995550cedf
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-07 04:56:40 +0900

    [Add] #73 - 추천 상황 키워드 API 연결에 필요한 파일 생성

---
+++ iOS-NOTTODO/iOS-NOTTODO/Network/API/AddMission/AddMissionAPI.swift
@@ -0,0 +1,8 @@
+//
+//  AddMissionAPI.swift
+//  iOS-NOTTODO
+//
+//  Created by 강윤서 on 2023/06/07.
+//
+
+import Foundation
---
+++ iOS-NOTTODO/iOS-NOTTODO/Network/DataModel/AddMission/RecommendSituationResponseDTO.swift
@@ -0,0 +1,8 @@
+//
+//  RecommendSituationResponseDTO.swift
+//  iOS-NOTTODO
+//
+//  Created by 강윤서 on 2023/06/07.
+//
+
+import Foundation
