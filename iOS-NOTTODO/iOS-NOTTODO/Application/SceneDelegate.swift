9556382b80f4373df760c032061e958aab6574e8
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-07 02:47:22 +0900

    [Fix] #69 - AddMissionViewController의 dismissButton 클릭 시 rootViewController로 화면전환하도록 구현

--- iOS-NOTTODO/iOS-NOTTODO/Application/SceneDelegate.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Application/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let window = UIWindow(windowScene: windowScene)
             window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
            
-            let rootViewController = HomeViewController()
+            let rootViewController = ValueOnboardingViewController()
             let navigationController = UINavigationController(rootViewController: rootViewController)
             navigationController.isNavigationBarHidden = true
             window.rootViewController = navigationController
