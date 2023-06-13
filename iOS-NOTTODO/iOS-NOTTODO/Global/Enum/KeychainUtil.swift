33ae5949ac7d6ebd267f73941e536469cee42f03
jeongdung-eo <jeuniis2@gmail.com>
2023-06-14 01:49:37 +0900

    [Add] #62 - userDefault 클래스 추가

---
+++ iOS-NOTTODO/iOS-NOTTODO/Global/Enum/KeychainUtil.swift
@@ -0,0 +1,36 @@
+//
+//  KeychainUtil.swift
+//  iOS-NOTTODO
+//
+//  Created by JEONGEUN KIM on 2023/06/14.
+//
+
+import Foundation
+
+public final class KeychainUtil {
+
+    static func setSocialToken(_ token: String) {
+        UserDefaults.standard.setValue(token, forKey: DefaultKeys.socialToken)
+    }
+    static func setAccessToken(_ token: String) {
+        UserDefaults.standard.setValue(token, forKey: DefaultKeys.accessToken)
+    }
+    static func setString(_ value: String?, forKey key: String) {
+        UserDefaults.standard.setValue(value, forKey: key)
+    }
+    static func setBool(_ value: Bool?, forKey key: String) {
+        UserDefaults.standard.set(value, forKey: key)
+    }
+    static func getSocialToken() -> String {
+        UserDefaults.standard.string(forKey: DefaultKeys.socialToken) ?? ""
+    }
+    static func getAccessToken() -> String {
+        UserDefaults.standard.string(forKey: DefaultKeys.accessToken) ?? ""
+    }
+    static func getUsername() -> String {
+        UserDefaults.standard.string(forKey: DefaultKeys.name) ?? "익명의 도전자"
+    }
+    static func getEmail() -> String {
+        UserDefaults.standard.string(forKey: DefaultKeys.email) ?? "연동된 이메일 정보가 없습니다"
+    }
+}
