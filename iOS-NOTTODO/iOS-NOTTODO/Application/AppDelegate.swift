09e75c07769a4d07128aee8603848d1d4a8b9a9b
minseo205 <rlaalstj312@naver.com>
2023-06-13 22:33:55 +0900

    [Feat] #77 - 자동 로그인 구현

--- iOS-NOTTODO/iOS-NOTTODO/Application/AppDelegate.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Application/AppDelegate.swift
@@ -6,7 +6,12 @@
 //
 
 import UIKit
+
 import KakaoSDKCommon
+import KakaoSDKAuth
+import KakaoSDKUser
+
+import AuthenticationServices
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,48 +23,49 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         KakaoSDK.initSDK(appKey: "f06c671df540ff4a8f8275f453368748")
         
-//        let accessToken = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.accessToken)
-//
-//        if accessToken != nil {
-//            if UserDefaults.standard.bool(forKey: Const.UserDefaultsKey.isAppleLogin) {
-//                // 애플 로그인으로 연동되어 있을 때 애플 ID와의 연동상태 확인
-//                let appleIDProvider = ASAuthorizationAppleIDProvider()
-//                appleIDProvider.getCredentialState(forUserID: UserDefaults.standard.string(forKey: Const.UserDefaultsKey.userID) ?? "") { credentialState, _ in
-//                    switch credentialState {
-//                    case .authorized:
-//                        print("해당 ID는 연동되어있습니다.")
-//                        self.isLogin = true
-//                    case .revoked:
-//                        print("해당 ID는 연동되어있지않습니다.")
-//                        self.isLogin = false
-//                    case .notFound:
-//                        print("해당 ID를 찾을 수 없습니다.")
-//                        self.isLogin = false
-//                    default:
-//                        break
-//                    }
-//                }
-//            } else {
-//                if AuthApi.hasToken() {
-//                    UserApi.shared.accessTokenInfo { _, error in
-//                        if let error = error {
-//                            if let sdkError = error as? SdkError, sdkError.isInvalidTokenError() == true {
-//                                self.isLogin = false
-//                            }
-//                        } else {
-//                            // 토큰 유효성 확인한 경우
-//                            self.isLogin = true
-//                        }
-//                    }
-//                } else {
-//                    // 유효한 토큰이 없는 경우
-//                    self.isLogin = false
-//                }
-//            }
-//        } else {
-//            // access token 이 없는 경우
-//            self.isLogin = false
-//        }
+        let kakaoAccessToken = UserDefaults.standard.string(forKey: "KakaoAccessToken")
+
+        if kakaoAccessToken != nil {
+            if UserDefaults.standard.bool(forKey: "isAppleLogin") {
+                
+                // 애플 연동상태 확인
+                let appleIDProvider = ASAuthorizationAppleIDProvider()
+                appleIDProvider.getCredentialState(forUserID: UserDefaults.standard.string(forKey: "AppleAccessToken") ?? "") { credentialState, _ in
+                    switch credentialState {
+                    case .authorized:
+                        print("해당 ID는 연동되어있습니다.")
+                        self.isLogin = true
+                    case .revoked:
+                        print("해당 ID는 연동되어있지않습니다.")
+                        self.isLogin = false
+                    case .notFound:
+                        print("해당 ID를 찾을 수 없습니다.")
+                        self.isLogin = false
+                    default:
+                        break
+                    }
+                }
+            } else {
+                if AuthApi.hasToken() {
+                    UserApi.shared.accessTokenInfo { _, error in
+                        if let error = error {
+                            if let sdkError = error as? SdkError, sdkError.isInvalidTokenError() == true {
+                                self.isLogin = false
+                            }
+                        } else {
+                            // 토큰 유효성 확인한 경우
+                            self.isLogin = true
+                        }
+                    }
+                } else {
+                    // 유효한 토큰이 없는 경우
+                    self.isLogin = false
+                }
+            }
+        } else {
+            // access token 이 없는 경우
+            self.isLogin = false
+        }
         
         return true
     }
