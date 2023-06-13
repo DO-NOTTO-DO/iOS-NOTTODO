f3fc3d0d144ea89dec35e5d72b8d95a070785541
jeongdung-eo <jeuniis2@gmail.com>
2023-06-14 02:16:50 +0900

    [Del] #62 - 불필요한 코드 삭제

--- iOS-NOTTODO/iOS-NOTTODO/Network/Base/NetworkConstant.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Network/Base/NetworkConstant.swift
@@ -11,6 +11,4 @@ struct NetworkConstant {
     static let noTokenHeader = ["Content-Type": "application/json"]
     static let hasTokenHeader = ["Content-Type": "application/json",
                                  "Authorization": "\(KeychainUtil.getAccessToken())"]
-    
-    static var accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiLsoJXrkaXslrRAbm90dG9kby5jb20iLCJpYXQiOjE2ODQxNDM4ODAsImV4cCI6MTY4NjE0Mzg4MH0.B5mWTVA01jT24q9LIW01ZXhgweC7-UydQrfev9Iovh4"
 }
