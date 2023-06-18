78fb23323c0a0f09337fe2d006eb0bed3de0e2d4
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-19 03:47:16 +0900

    [Add] #93 - 프로토콜 내 텍스트를 전달하는 클로저와 메소드 생성 - setCellData: 뷰 컨트롤러가 가지고 있는 데이터에 따라 셀의 UI를 변경해 주는 메소드 - missionTextData: 현재 셀의 텍스트를 뷰컨트롤러로 넘겨주는 클로저

--- iOS-NOTTODO/iOS-NOTTODO/Presentation/AddMission/Protocol/AddMissionProtocol.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/AddMission/Protocol/AddMissionProtocol.swift
@@ -9,5 +9,7 @@ import UIKit
 
 protocol AddMissionMenu {
     func setFoldState(_ state: FoldState)
+    func setCellData(_ text: String)
     var missionCellHeight: ((CGFloat) -> Void)? { get set }
+    var missionTextData: ((String) -> Void)? { get set }
 }
