8b6d577c672dda882b932f47cebe917cd1a6b3bf
minseo205 <rlaalstj312@naver.com>
2023-06-25 19:48:40 +0900

    [Fix] #124 - 타이틀 및 서브타이틀 수정

--- iOS-NOTTODO/iOS-NOTTODO/Global/Literals/Strings.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Global/Literals/Strings.swift
@@ -10,12 +10,13 @@ struct I18N {
     /// Auth
     
     static let loginMain = """
-                        나만을 위한 추천과
-                        안전한 데이터 보관의 시작!
+                        이제 오늘의 낫투두를 세우고
+                        변화된 삶을 느껴보세요!
                         """
     static let loginSub = """
-                        계정을 연동하면 언제 어디서든
-                        낫투두 기록을 관리할 수 있어요.
+                        계정을 연동하면
+                        안전하게 데이터를 보관하고
+                        어디서든 낫투두 기록을 관리할 수 있어요.
                         """
     static let kakaoLogin = "카카오 로그인"
     static let appleLogin = "Apple로 로그인"
