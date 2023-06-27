96195477b4042c91dc1fb53eb1fedce2a4bba511
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-28 03:27:36 +0900

    [Feat] #131 - shadow 설정 메소드 추가

--- iOS-NOTTODO/iOS-NOTTODO/Global/Extensions/CALayer+.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Global/Extensions/CALayer+.swift
@@ -5,4 +5,20 @@
 //  Created by 강윤서 on 2023/06/28.
 //
 
-import Foundation
+import UIKit
+
+extension CALayer {
+    func makeShadow(color: UIColor, alpha: Float, x: CGFloat, y: CGFloat, blur: CGFloat, spread: CGFloat) {
+        masksToBounds = false
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / UIScreen.main.scale
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let rect = bounds.insetBy(dx: -spread, dy: -spread)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+}
