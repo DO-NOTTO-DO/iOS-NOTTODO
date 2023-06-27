15aa8de662d63a71bec8247113e9da1e90a0c908
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-28 03:26:26 +0900

    [Chore] #131 - 토스트 메세지에 필요한 이미지 추가 및 modal 뷰 폴더 이동

---
+++ iOS-NOTTODO/iOS-NOTTODO/Global/Extensions/CALayer+.swift
@@ -0,0 +1,8 @@
+//
+//  CALayer+.swift
+//  iOS-NOTTODO
+//
+//  Created by 강윤서 on 2023/06/28.
+//
+
+import Foundation
---
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/Common/Toast/NottodoToastView.swift
@@ -0,0 +1,20 @@
+//
+//  NottodoToastView.swift
+//  iOS-NOTTODO
+//
+//  Created by 강윤서 on 2023/06/27.
+//
+
+import UIKit
+
+class NottodoToastView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
