25ffec1b41827299b5ebf3c382a0382e327bc651
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-28 03:29:52 +0900

    [Design] #131 - toastView UI 구현

--- iOS-NOTTODO/iOS-NOTTODO/Presentation/Common/Toast/NottodoToastView.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/Common/Toast/NottodoToastView.swift
@@ -7,14 +7,62 @@
 
 import UIKit
 
-class NottodoToastView: UIView {
+import SnapKit
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class NottodoToastView: UIView {
+    
+    // MARK: - Properties
+    
+    private var message: String?
+    
+    // MARK: - UI Components
+    
+    private let errorImage = UIImageView()
+    private let errorMessage = UILabel()
+    
+    init(message: String, viewController: UIViewController) {
+        self.message = message
+        super.init(frame: .zero)
+        setUI()
+        setLayout()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension NottodoToastView {
+    private func setUI() {
+        backgroundColor = .white
+        layer.cornerRadius = 12
+        layer.makeShadow(color: .black, alpha: 0.25, x: 0, y: 4, blur: 30, spread: 0)
+        isUserInteractionEnabled = false
+        
+        errorImage.image = .icToastError
+        
+        errorMessage.do {
+            $0.text = message
+            $0.textColor = .gray2
+            $0.numberOfLines = 0
+            $0.textAlignment = .left
+            $0.font = .Pretendard(.regular, size: 14)
+        }
+    }
+    
+    private func setLayout() {
+        addSubviews(errorImage, errorMessage)
+        
+        errorImage.snp.makeConstraints {
+            $0.size.equalTo(30)
+            $0.top.equalToSuperview().inset(18)
+            $0.leading.equalToSuperview().inset(16)
+        }
+        
+        errorMessage.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(11)
+            $0.leading.equalTo(errorImage.snp.trailing).offset(6)
+        }
+    }
 }
