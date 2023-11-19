a81c10ba1f645e9ad26b72d757edef19c0caf052
jeongdung-eo <jeuniis2@gmail.com>
2023-11-19 16:25:15 +0900

    [Refactor] #197 - 미션 디테일 VC 로직 변경

--- iOS-NOTTODO/iOS-NOTTODO/Presentation/Home/ViewControllers/MissionDetailViewController.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/Home/ViewControllers/MissionDetailViewController.swift
@@ -10,158 +10,199 @@ import UIKit
 import Then
 import SnapKit
 
+import UIKit
+
+import Then
+import SnapKit
+
 final class MissionDetailViewController: UIViewController {
     
     // MARK: - Properties
+    
+    typealias CellRegistration = UICollectionView.CellRegistration
+    typealias FooterRegistration = UICollectionView.SupplementaryRegistration
+    typealias Item = MissionDetailResponseDTO
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Item>
+    typealias SnapShot = NSDiffableDataSourceSnapshot<Section, Item>
+    
+    enum Section {
+        case main
+    }
+    
+    private var dataSource: DataSource?
+    private var detailModel: MissionDetailResponseDTO?
+    var userId: Int?
     
     var deleteClosure: (() -> Void)?
     var moveDateClosure: ((_ date: String) -> Void)?
     
     private lazy var safeArea = self.view.safeAreaLayoutGuide
-    enum Section {
-        case mission
-    }
-    typealias Item = AnyHashable
-    var dataSource: UICollectionViewDiffableDataSource<Section, Item>! = nil
-    var detailModel: [MissionDetailResponseDTO] = []
-    var userId: Int?
     
     // MARK: - UI Components
     
+    private let headerView = DetailHeaderView()
     private let deleteButton = UIButton(configuration: .filled())
-    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
-    private let completeButton = UIButton()
+    private var collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
     
     // MARK: - Life Cycle
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         guard let id = self.userId else { return }
         requestDailyMissionAPI(id: id)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        register()
+        
         setUI()
         setLayout()
         setupDataSource()
-        reloadData()
+        setSnapShot()
     }
 }
 
 // MARK: - Methods
 
 extension MissionDetailViewController {
-    private func register() {
-        collectionView.register(MissionDetailCollectionViewCell.self, forCellWithReuseIdentifier: MissionDetailCollectionViewCell.identifier)
-        collectionView.register(DetailHeaderReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: DetailHeaderReusableView.identifier)
-        collectionView.register(DetailFooterReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: DetailFooterReusableView.identifier)
-    }
+    
     private func setUI() {
+        
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.25) {
             UIView.animate(withDuration: 0.5) {
                 self.view.backgroundColor = .black.withAlphaComponent(0.6)
             }
         }
         
-        deleteButton.do {
-            $0.configuration?.title = I18N.detailDelete
-            $0.configuration?.cornerStyle = .capsule
-            $0.configuration?.attributedTitle?.font = .Pretendard(.semiBold, size: 16)
-            $0.configuration?.baseBackgroundColor = .black
-            $0.configuration?.baseForegroundColor = .white
-            $0.addTarget(self, action: #selector(deleteBtnTapped), for: .touchUpInside)
-        }
-        collectionView.do {
+        headerView.do {
             $0.backgroundColor = .white
             $0.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
             $0.layer.cornerRadius = 10
-            $0.bounces = false
-            $0.isScrollEnabled = false
         }
+        
+        collectionView.do {
+            $0.collectionViewLayout = layout()
+            $0.bounces = false
+            $0.backgroundColor = .white
+            $0.showsVerticalScrollIndicator = false
+            $0.contentInset = .init(top: -30, left: 0, bottom: 88, right: 0)
+        }
+        
+        deleteButton.do {
+            
+            var config = UIButton.Configuration.filled()
+            config.title = I18N.detailDelete
+            config.cornerStyle = .capsule
+            config.baseBackgroundColor = .black
+            config.baseForegroundColor = .white
+            config.attributedTitle?.font = .Pretendard(.semiBold, size: 16)
+            
+            $0.configuration = config
+            $0.addTarget(self, action: #selector(deleteBtnTapped), for: .touchUpInside)
+        }
+        
+        configureHeaderView()
     }
     
     private func setLayout() {
-        view.addSubviews(collectionView, deleteButton)
+        view.addSubviews(headerView, collectionView, deleteButton)
         
-        collectionView.snp.makeConstraints {
-            $0.height.equalTo(getDeviceHeight()*0.88)
-            $0.directionalHorizontalEdges.equalTo(safeArea)
-            $0.bottom.equalToSuperview()
+        headerView.snp.makeConstraints {
+            $0.top.equalTo(safeArea).inset(58)
+            $0.horizontalEdges.equalTo(safeArea)
+            $0.height.equalTo(74)
         }
+        
         deleteButton.snp.makeConstraints {
-            $0.directionalHorizontalEdges.equalToSuperview().inset(15)
+            $0.horizontalEdges.equalToSuperview().inset(15)
             $0.height.equalTo(50)
             $0.bottom.equalTo(safeArea).inset(10)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom)
+            $0.horizontalEdges.bottom.equalTo(safeArea)
         }
     }
     
     // MARK: - Data
     
     private func setupDataSource() {
-        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MissionDetailCollectionViewCell.identifier, for: indexPath) as? MissionDetailCollectionViewCell else {return UICollectionViewCell()}
-            cell.configure(model: item as! MissionDetailResponseDTO)
-            return cell
+        
+        let cellRegistration = CellRegistration<MissionDetailCollectionViewCell, Item> {cell, _, item in
+            cell.configure(model: item)
+        }
+        
+        let footerRegistration = FooterRegistration<DetailFooterView>(elementKind: UICollectionView.elementKindSectionFooter) { footerView, _, _ in
+            footerView.footerClosure = {
+                let modalViewController = DetailCalendarViewController()
+                modalViewController.detailModel = self.detailModel
+                modalViewController.modalPresentationStyle = .overFullScreen
+                modalViewController.modalTransitionStyle = .crossDissolve
+                guard let id = self.userId else {return}
+                modalViewController.userId = id
+                modalViewController.movedateClosure = { [weak self] date in
+                    self?.dismiss(animated: true)
+                    self?.moveDateClosure?(date)
+                }
+                self.present(modalViewController, animated: false)
+            }
+        }
+        
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration,
+                                                                for: indexPath,
+                                                                item: item)
         })
+        
+        dataSource?.supplementaryViewProvider = { collectionView, _, indexPath in
+            return collectionView.dequeueConfiguredReusableSupplementary(using: footerRegistration,
+                                                                         for: indexPath)
+        }
     }
     
-    private func reloadData() {
-        var snapShot = NSDiffableDataSourceSnapshot<Section, Item>()
-        defer {
-            dataSource.apply(snapShot, animatingDifferences: false)
+    private func configureHeaderView() {
+        headerView.cancelClosure = {
+            self.view.alpha = 0
+            self.dismiss(animated: true) {
+                AmplitudeAnalyticsService.shared.send(event: AnalyticsEvent.Detail.closeDetailMission)
+            }
         }
-        snapShot.appendSections([.mission])
-        snapShot.appendItems(detailModel, toSection: .mission)
         
-        dataSource.supplementaryViewProvider = { (collectionView, kind, indexPath) in
-            if kind == UICollectionView.elementKindSectionHeader {
-                guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: DetailHeaderReusableView.identifier, for: indexPath) as? DetailHeaderReusableView else {return UICollectionReusableView()}
-                header.cancelClosure = {
-
-                    self.view.alpha = 0
-                    self.dismiss(animated: true)
-                    AmplitudeAnalyticsService.shared.send(event: AnalyticsEvent.Detail.closeDetailMission)
-                }
-                header.editClosure = {
-                    AmplitudeAnalyticsService.shared.send(event: AnalyticsEvent.Detail.clickEditMission(section: "detail"))
-                    
-                    let updateMissionViewController = AddMissionViewController()
-                    guard let rootViewController = self.presentingViewController as? UINavigationController else { return }
-                    updateMissionViewController.setMissionId(self.userId ?? 0)
-                    updateMissionViewController.setViewType(.update)
-                    self.dismiss(animated: true) {
-                        rootViewController.pushViewController(updateMissionViewController, animated: true)
-                    }
-                }
-                return header
-            } else {
-                guard let footer = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: DetailFooterReusableView.identifier, for: indexPath) as? DetailFooterReusableView else { return UICollectionReusableView() }
-                footer.footerClosure = {
-                    let modalViewController = DetailCalendarViewController()
-                    modalViewController.detailModel = self.detailModel
-                    modalViewController.modalPresentationStyle = .overFullScreen
-                    modalViewController.modalTransitionStyle = .crossDissolve
-                    guard let id = self.userId else {return}
-                    modalViewController.userId = id
-                    modalViewController.movedateClosure = { [weak self] date in
-                        self?.dismiss(animated: true)
-                        self?.moveDateClosure?(date)
-                    }
-                    self.present(modalViewController, animated: false)
-                }
-                return footer
+        headerView.editClosure = {
+            AmplitudeAnalyticsService.shared.send(event: AnalyticsEvent.Detail.clickEditMission(section: "detail"))
+            
+            let updateMissionViewController = AddMissionViewController()
+            guard let rootViewController = self.presentingViewController as? UINavigationController else { return }
+            updateMissionViewController.setMissionId(self.userId ?? 0)
+            updateMissionViewController.setViewType(.update)
+            self.dismiss(animated: true) {
+                rootViewController.pushViewController(updateMissionViewController, animated: true)
             }
         }
     }
     
+    private func setSnapShot() {
+        
+        var snapShot = SnapShot()
+        
+        if let detailModel = self.detailModel {
+            snapShot.appendSections([.main])
+            snapShot.appendItems([detailModel], toSection: .main)
+        }
+        
+        dataSource?.applySnapshotUsingReloadData(snapShot)
+    }
+    
     private func layout() -> UICollectionViewCompositionalLayout {
-        var config = UICollectionLayoutListConfiguration(appearance: .plain)
+        var config = UICollectionLayoutListConfiguration(appearance: .grouped)
+        config.backgroundColor = .white
         config.showsSeparators = false
-        config.headerMode = .supplementary
         config.footerMode = .supplementary
+        
         return UICollectionViewCompositionalLayout.list(using: config)
+        
     }
 }
 
@@ -169,53 +210,53 @@ extension MissionDetailViewController {
     
     @objc
     func deleteBtnTapped() {
-        AmplitudeAnalyticsService.shared.send(event: AnalyticsEvent.Detail.clickDeleteMission(section: "detail", title: self.detailModel[0].title, situation: self.detailModel[0].situation, goal: self.detailModel[0].goal, action: [self.detailModel[0].actions[0].name]))
+        
+        guard let data = detailModel else { return }
+        AmplitudeAnalyticsService.shared.send(event: AnalyticsEvent.Detail.clickDeleteMission(section: "detail", title: data.title, situation: data.situation, goal: data.goal, action: [data.actions[0].name]))
+        
         let modalViewController = HomeDeleteViewController()
         modalViewController.modalPresentationStyle = .overFullScreen
         modalViewController.modalTransitionStyle = .crossDissolve
         modalViewController.deleteClosure = {
             guard let id = self.userId else { return }
-            
             self.requestDeleteMission(id: id)
         }
+        
         present(modalViewController, animated: false)
     }
 }
 
 extension MissionDetailViewController {
+    
     func requestDailyMissionAPI(id: Int) {
-        HomeAPI.shared.getDailyDetailMission(id: id) { [weak self] result in
-            switch result {
-            case let .success(data):
-                if let missionData = data as? MissionDetailResponseDTO {
-                    print("data: \(missionData)")
-                    self?.detailModel = [missionData]
-                    self?.reloadData()
-                } else {
-                    print("Failed to cast data to MissionDetailResponseDTO")
-                }
-            case .pathErr:
-                print("pathErr")
-            case .serverErr:
-                print("serverErr")
-            case .networkFail:
-                print("networkFail")
-            case .requestErr:
-                print("networkFail")
-            }
+        HomeAPI.shared.getDailyDetailMission(id: id) { [weak self] response in
+            guard let self else { return }
+            guard let response = response else { return }
+            guard let data = response.data else { return }
+            
+            self.detailModel = data
+            self.setSnapShot()
         }
     }
     
     private func requestDeleteMission(id: Int) {
         HomeAPI.shared.deleteMission(id: id) { [weak self] _ in
-            for index in 0..<(self?.detailModel.count ?? 0) {
-                if self?.detailModel[index].id == id {
-                    self?.deleteClosure?()
-                    self?.reloadData()
-                    AmplitudeAnalyticsService.shared.send(event: AnalyticsEvent.Detail.completeDeleteMission(section: "detail", title: (self?.detailModel[index].title)!, situation: (self?.detailModel[index].situation)!, goal: self?.detailModel[index].goal ?? "", action: [self?.detailModel[index].actions[index].name ?? ""]))
-                    self?.dismiss(animated: true)
+            guard let self else { return }
+            guard let data = self.detailModel else { return }
+            if data.id == id {
+                self.deleteClosure?()
+                self.setSnapShot()
+                
+                AmplitudeAnalyticsService.shared.send(event: AnalyticsEvent.Detail.completeDeleteMission(section: "detail",
+                                                                                                         title: data.title,
+                                                                                                         situation: data.situation,
+                                                                                                         goal: data.goal,
+                                                                                                         action: data.actions.map { $0.name }))
+                
+                self.dismiss(animated: true) {
+                    
                     AmplitudeAnalyticsService.shared.send(event: AnalyticsEvent.Detail.closeDetailMission)
-                } else {}
+                }
             }
         }
     }
