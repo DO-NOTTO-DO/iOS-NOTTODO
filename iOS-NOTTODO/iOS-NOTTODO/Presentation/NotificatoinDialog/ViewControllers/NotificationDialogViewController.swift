110cbf83279b9d862c2644c381ded06d99bdcff2
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-11-23 17:42:54 +0900

    [Add] #206 - 이미지 추가

---
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/NotificatoinDialog/ViewControllers/NotificationDialogViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationDialogViewController.swift
+//  iOS-NOTTODO
+//
+//  Created by 강윤서 on 11/23/23.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class NotificationDialogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
