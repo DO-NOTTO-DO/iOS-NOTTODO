dabac045bee12a6737220eda0bb60201ed96ec0e
jeongdung-eo <jeuniis2@gmail.com>
2023-11-26 17:00:16 +0900

    [Fix] #198 - 코드 리뷰 반영 2

--- iOS-NOTTODO/iOS-NOTTODO/Presentation/Achievement/ViewControllers/DetailAchievementViewController.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Presentation/Achievement/ViewControllers/DetailAchievementViewController.swift
@@ -148,7 +148,8 @@ extension DetailAchievementViewController {
 extension DetailAchievementViewController {
     
     private func requestDetailAPI(date: String) {
-        HomeAPI.shared.getDailyMission(date: date) { response in
+        HomeAPI.shared.getDailyMission(date: date) { [weak self] response in
+            guard let self else { return }
             guard let response = response else { return }
             guard let data = response.data else { return }
             let missionList = data
