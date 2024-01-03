95a521f25cdcd72c17c1bdc308827b803468e01a
jeongdung-eo <jeuniis2@gmail.com>
2024-01-03 16:15:08 +0900

    [Add] #211 - 공통 알리 모달 icon 추가

--- iOS-NOTTODO/iOS-NOTTODO/Global/Extensions/Design/UIImage+.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Global/Extensions/Design/UIImage+.swift
@@ -23,6 +23,8 @@ extension UIImage {
     static var calendarRight: UIImage { UIImage(named: "btn_calendar_right")! }
     static var checkboxFill: UIImage { UIImage(named: "btn_checkbox_active")! }
     static var checkbox: UIImage { UIImage(named: "btn_checkbox_inactive")! }
+    static var deprecatedCheckBoxFill: UIImage { UIImage(named: "btn_common_active")! }
+    static var deprecatedCheckBox: UIImage { UIImage(named: "btn_common_inactive")! }
     
     // home
     
@@ -56,6 +58,7 @@ extension UIImage {
     static var icSNS: UIImage { UIImage(named: "ic_snsmessage")! }
     static var icTrashbin: UIImage { UIImage(named: "ic_trashbin")! }
     static var icToastError: UIImage { UIImage(named: "ic_toast_error")! }
+    static var icStarbucks: UIImage { UIImage(named: "ic_starbucks")! }
     
     // image
     
