c729136957b45bed4114c81ad11988b04909235b
jeongdung-eo <jeuniis2@gmail.com>
2024-01-03 16:15:24 +0900

    [Add] #211 - 공통 모달 string 리터럴 추가

--- iOS-NOTTODO/iOS-NOTTODO/Global/Literals/Strings.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Global/Literals/Strings.swift
@@ -162,6 +162,9 @@ struct I18N {
     static let delete = "삭제하기"
     static let deleteModalTitle = "삭제하시겠습니까?"
     static let deleteModalSubtitle = "한 번 삭제하면 되돌릴 수 없어요."
+    static let formButton = "이정도야 쉽지!"
+    static let deprecatedTitle = "더 이상 보지 않기"
+    static let close = "닫기"
 
     /// home
     static let subText = "*달성 가능한 계획을 위해 다가올 일주일만 선택할 수 있어요"
