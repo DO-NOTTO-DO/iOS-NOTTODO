1f8c02d48694fcd25b692883e6427d220037d355
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-02-16 15:08:37 +0900

    [Chore] #232 - 기기 대응을 위한 상수 추가 및 중복값 상수로 변경

--- iOS-NOTTODO/iOS-NOTTODO/Global/Extensions/adjust+.swift
+++ iOS-NOTTODO/iOS-NOTTODO/Global/Extensions/adjust+.swift
@@ -5,4 +5,25 @@
 //  Created by 강윤서 on 2/16/24.
 //
 
-import Foundation
+import UIKit
+
+extension CGFloat {
+    var adjusted: CGFloat {
+        let ratio: CGFloat = Numbers.width / 375
+        return ratio * CGFloat(self)
+    }
+}
+
+extension Int {
+    var adjusted: CGFloat {
+        let ratio: CGFloat = (Numbers.width) / 375
+        return ratio * CGFloat(self)
+    }
+}
+
+extension Double {
+    var adjusted: CGFloat {
+        let ratio: CGFloat = (Numbers.width) / 375
+        return ratio * CGFloat(self)
+    }
+}
