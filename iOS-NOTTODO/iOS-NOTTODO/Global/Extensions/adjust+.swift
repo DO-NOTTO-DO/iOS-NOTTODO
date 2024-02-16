3643ee11633b898e4bd71ca0367961d8874b51c1
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-02-16 15:07:05 +0900

    [Add] #232 - 성취 뷰 하단 이미지 추가

---
+++ iOS-NOTTODO/iOS-NOTTODO/Global/Extensions/adjust+.swift
@@ -0,0 +1,8 @@
+//
+//  adjust+.swift
+//  iOS-NOTTODO
+//
+//  Created by 강윤서 on 2/16/24.
+//
+
+import Foundation
---
+++ iOS-NOTTODO/iOS-NOTTODO/Global/Literals/Numbers.swift
@@ -0,0 +1,8 @@
+//
+//  Numbers.swift
+//  iOS-NOTTODO
+//
+//  Created by 강윤서 on 2/16/24.
+//
+
+import Foundation
