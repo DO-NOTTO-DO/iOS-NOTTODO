1ef50ed0cfb44f0ee16507824affec724f1504e8
jeongdung-eo <jeuniis2@gmail.com>
2024-02-28 15:21:49 +0900

    [Feat] #226 - coordinator type 구현

---
+++ iOS-NOTTODO/iOS-NOTTODO/Coordinator/Type/CoordinatorType.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorType.swift
+//  iOS-NOTTODO
+//
+//  Created by JEONGEUN KIM on 2/18/24.
+//
+
+import Foundation
+
+@frozen enum CoordinatorType {
+    case app
+    case update
+    case auth
+    case home
+    case mypage
+    case achievement
+}
