46dde7c628b759c72c192a5b989d0d570db85e50
jeongdung-eo <jeuniis2@gmail.com>
2024-02-28 15:37:56 +0900

    [Feat] #226 - Coordinator Factory 구현

---
+++ iOS-NOTTODO/iOS-NOTTODO/Coordinator/Factory/CoordinatorFactory.swift
@@ -0,0 +1,85 @@
+//
+//  CoordinatorFactory.swift
+//  iOS-NOTTODO
+//
+//  Created by JEONGEUN KIM on 2/18/24.
+//
+
+import UIKit
+
+/*
+ CoordinatorFactory
+ - description: 코디네이터 생성을 담당
+ */
+
+protocol CoordinatorFactory {}
+final class CoordinatorFactoryImpl: CoordinatorFactory {}
+
+extension CoordinatorFactory {
+    func makeUpdateCoordinator(
+        _ navigationController: UINavigationController,
+        coordinatorFactory: CoordinatorFactory,
+        viewControllerFactory: ViewControllerFactory
+    ) -> UpdateCoordinator {
+        let coordinator = UpdateCoordinatorImpl(
+            navigationController,
+            coordinatorFactory: coordinatorFactory,
+            viewControllerFactory: viewControllerFactory
+        )
+        return coordinator
+    }
+    
+    func makeAuthCoordinator(
+        _ navigationController: UINavigationController,
+        coordinatorFactory: CoordinatorFactory,
+        viewControllerFactory: ViewControllerFactory
+    ) -> AuthCoordinator {
+        let coordinator = AuthCoordinatorImpl(
+            navigationController,
+            coordinatorFactory: coordinatorFactory,
+            viewControllerFactory: viewControllerFactory
+           )
+        return coordinator
+    }
+    
+    func makeHomeCoordinator(
+        _ navigationController: UINavigationController,
+        coordinatorFactory: CoordinatorFactory,
+        viewControllerFactory: ViewControllerFactory
+    ) -> HomeCoordinator {
+        let coordinator = HomeCoordinatorImpl(
+            navigationController,
+            coordinatorFactory: coordinatorFactory,
+            viewControllerFactory: viewControllerFactory
+        )
+        return coordinator
+    }
+    
+    func makeAchieveCoordinator(
+        _ navigationController: UINavigationController,
+        coordinatorFactory: CoordinatorFactory,
+        viewControllerFactory: ViewControllerFactory
+    )
+    -> AchieveCoordinator {
+        let coordinator = AchieveCoordinatorImpl(
+            navigationController,
+            coordinatorFactory: coordinatorFactory,
+            viewControllerFactory: viewControllerFactory
+        )
+        return coordinator
+    }
+    
+    func makeMypageCoordinator(
+        _ navigationController: UINavigationController,
+        coordinatorFactory: CoordinatorFactory,
+        viewControllerFactory: ViewControllerFactory
+    )
+    -> MypageCoordinator {
+        let coordinator = MypageCoordinatorImpl(
+            navigationController,
+            coordinatorFactory: coordinatorFactory,
+            viewControllerFactory: viewControllerFactory
+        )
+        return coordinator
+    }
+}
