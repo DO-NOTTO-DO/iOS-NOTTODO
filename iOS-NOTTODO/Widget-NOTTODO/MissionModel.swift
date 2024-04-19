713415da8ffa99623a8d04221d61e453a1a022f0
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-04-19 14:13:06 +0900

    [Add] #252 - 더미데이터 추가

--- iOS-NOTTODO/Widget-NOTTODO/MissionModel.swift
+++ iOS-NOTTODO/Widget-NOTTODO/MissionModel.swift
@@ -5,4 +5,23 @@
 //  Created by 강윤서 on 4/14/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct MissionModel: Identifiable {
+    var id = UUID().uuidString
+    var missionTitle: String
+    @State var isCompleted: Bool = false
+}
+
+class MissionDataModel {
+    static let shared = MissionDataModel()
+    
+    var model: [MissionModel] = [
+        .init(missionTitle: "바마바ㅏ어람니어리 ㅏㅓㅏ어라너ㅏㅇ러라언", isCompleted: true),
+        .init(missionTitle: "바마바ㅏ어람니어리 ㅏㅓㅏ어라너ㅏㅇ러라언"),
+        .init(missionTitle: "바마바ㅏ어람니어리 ㅏㅓㅏ어라너ㅏㅇ러라언"),
+        .init(missionTitle: "바마바ㅏ어람니어리 ㅏㅓㅏ어라너ㅏㅇ러라언"),
+        .init(missionTitle: "바마바ㅏ어람니어리 ㅏㅓㅏ어라너ㅏㅇ러라언"),
+        .init(missionTitle: "바마바ㅏ어람니어리 ㅏㅓㅏ어라너ㅏㅇ러라언")
+    ]
+}
