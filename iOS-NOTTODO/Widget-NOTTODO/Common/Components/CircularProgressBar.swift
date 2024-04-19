d261a5cc91f2b3031e75b719b006e4da1797ad49
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-04-19 14:10:39 +0900

    [Add] #252 - Circular ProgressBar 생성

--- iOS-NOTTODO/Widget-NOTTODO/Common/Components/CircularProgressBar.swift
+++ iOS-NOTTODO/Widget-NOTTODO/Common/Components/CircularProgressBar.swift
@@ -5,4 +5,35 @@
 //  Created by 강윤서 on 4/17/24.
 //
 
-import Foundation
+import WidgetKit
+import SwiftUI
+
+struct CircularProgressBarView: View {
+    
+    @State var percent = 0.0
+    var size: CGFloat
+    var lineWidth: CGFloat
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: lineWidth)
+                .frame(width: size, height: size)
+                .foregroundColor(.gray3)
+            
+            Circle()
+                .trim(from: 0, to: percent)
+                .stroke(style: StrokeStyle(lineWidth: lineWidth, lineCap: .butt))
+                .frame(width: size, height: size)
+                .foregroundColor(.green2)
+                .rotationEffect(.degrees(-90))
+                .scaleEffect(x: -1)
+        }
+    }
+}
+
+#Preview(as: .systemSmall) {
+    Widget_NOTTODO()
+} timeline: {
+    SimpleEntry(lastThreeTask: [])
+}
